592d58c8594ef2956e57cedfd1a88b4d7361baa6
Loïc Herblot <neon.alpha@training.kplr.fr>
2023-06-26 12:22:42 +0000

    Ajout de la fonction modifier_eleve

--- gestion_eleves.py
+++ gestion_eleves.py
@@ -3,3 +3,12 @@ def ajouter_eleve():
     nom_eleve = input("Entrez le nom de l'élève : ")
     eleves[id_eleve] = nom_eleve
     print("Élève ajouté avec succès !")
+
+def modifier_eleve():
+    id_eleve = input("Entrez l'identifiant de l'élève à modifier : ")
+    if id_eleve in eleves:
+        nouveau_nom = input("Entrez le nouveau nom de l'élève : ")
+        eleves[id_eleve] = nouveau_nom
+        print("Élève mis à jour avec succès !")
+    else:
+        print("L'élève n'existe pas.")
